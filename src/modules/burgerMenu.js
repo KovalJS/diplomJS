69d0dff1d9b6a56ad8a48c58f3057e9cca4dc1cb
Koval Alexander <x-sania@mail.ru>
2019-12-05 15:50:09 +0500

    burgerMenu при скроле вниз прилепает к верху

--- src/modules/burgerMenu.js
+++ src/modules/burgerMenu.js
@@ -1,15 +1,36 @@
 const burgerMenu = () => {
-    const hiddenLarge = document.querySelector('.hidden-large');
-
+    const hiddenLarge = document.querySelector('.hidden-large'),
+        topMenu = document.querySelector('.top-menu');
+    let head = document.querySelector('.head'),
+        headHiight = head.clientHeight,
+        burgerWidthWindow = document.documentElement.clientWidth;
+   
     window.addEventListener('resize', () => {
-        let burgerWidthWindow = document.documentElement.clientWidth;
+        burgerWidthWindow = document.documentElement.clientWidth;
+        headHiight = head.clientHeight;
         
         if (burgerWidthWindow < 768) {
             hiddenLarge.style.display = 'block';
         } else {
             hiddenLarge.style.display = 'none';
         }
-    });   
+    });  
+
+    window.addEventListener('scroll', () => {
+        let windowHeight = document.documentElement.scrollTop;
+
+        if (windowHeight >= headHiight && burgerWidthWindow < 768) {
+            topMenu.style.cssText = `
+                position: fixed;
+                top: 0;
+                z-index: 3000;
+            `;
+        } else if (windowHeight < headHiight && burgerWidthWindow < 768) {
+            topMenu.style.cssText = `
+                position: none;
+            `;
+        }
+    });
 };
 
 export default burgerMenu;
